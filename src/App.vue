8b3f62c3ac184de8055f6b7162da5a059c0684c7
Muhammad Fachrur Riza <mfriza69@gmail.com>
2023-06-07 19:53:55 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Navbar />
+  <Body/>
+</template>
+
+<script>
+  import Navbar from "./components/Navbar.vue";
+  import Body from "./components/Body.vue";
+
+  export default {
+    name: "App",
+    components: { Navbar, Body },
+  };
+</script>
